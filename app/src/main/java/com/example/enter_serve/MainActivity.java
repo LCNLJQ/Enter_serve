72ba17742e9b4cf8220b68cf199ef1d492f682eb
Your Name <you@example.com>
2019-12-16 11:33:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/enter_serve/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.enter_serve;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
